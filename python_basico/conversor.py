ba70b90f11e7c73ca308b23316f00151a9fe5eda
Marco Velasco <marco1velasco@gmail.com>
2021-05-08 14:13:50 -0500

    reducción de los decimales usando round

--- python_basico/conversor.py
+++ python_basico/conversor.py
@@ -1,5 +1,6 @@
 pesos = float(input('¿cuántos pesos mexicanos tienes?'))
 valor_dolar = 19.90
 dolares = pesos / valor_dolar
+dolares = round(dolares, 2)
 dolares = str(dolares)
 print('Tienes $' + dolares + 'dólares')
