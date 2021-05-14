6193023f10968659a1d30c8903759c15656b7078
Marco Velasco <marco1velasco@gmail.com>
2021-05-14 14:32:00 -0500

    acomodo de archivos

--- python_basico/for.py
+++
@@ -1,9 +0,0 @@
-# contador = 1
-# print(contador)
-# while contador < 1000:
-#     contador += 1
-#     print(contador)
-
-
-for contador in range(1001):
-    print(contador)
--- python_basico/funciones.py
+++
@@ -1,7 +0,0 @@
-def imprimir_mensaje():
-    print('mensaje especial')
-    print('¡Estoy aprendiendo a usar funciones')
-
-imprimir_mensaje()
-imprimir_mensaje()
-imprimir_mensaje()
